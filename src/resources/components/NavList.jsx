accfe5650f51ce38b16472b73c91b04fdd786f9b
gitgut0110 <jsimms0110@gmail.com>
2018-09-13 13:06:33 +0200

    broken going on lunch

--- src/resources/components/NavList.jsx
+++ src/resources/components/NavList.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-// import Home from "./Home";
+import { Route, NavLink, BrowserRouter } from "react-router-dom";
+import Home from "./Home";
 // import Blog from "./Blog";
 // import Contact from "./Contact";
-
+import Details from "./CountryDetails";
 
 const NavList = props => {
-    const mappedList = props.list.map((el,i) =>
-    
-    <a 
-        key={i} 
-        to={el}
+  const mappedList = props.list.map((el, i) => (
+    <React.Fragment>
+      <NavLink
+        key={i}
+        to={"/" + el}
         className="list-group-item list-group-item-action"
         onClick={() => props.clicked(i)}
-    >{el}</a>
-    
-    )
-    return (
-      <div className="list-group nav-list">
-        {mappedList}
+      >
+        {el}
+      </NavLink>
+      <Route path={"/" + el} component={Details} />
+    </React.Fragment>
+  ));
+  return (
+    <BrowserRouter>
+      <div className="pseudoNav">
+        <NavLink to="/">Home</NavLink>
+
+        <Route exact path="/" component={Details} />
       </div>
-    );
-  
-}
+    </BrowserRouter>
+    // <div className="list-group nav-list">{mappedList}</div>
+  );
+};
 
 export default NavList;
